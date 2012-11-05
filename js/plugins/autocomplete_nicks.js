2b44f8d70d1f055fef795ce8d1acbc0cb84540b8
Pablo Villalba <pablo@teambox.com>
2012-11-05 16:04:08 +0100

    Autocomplete usernames

---
+++ js/plugins/autocomplete_nicks.js
@@ -0,0 +1,16 @@
+(function () {
+
+  function bindAutocompleter() {
+    $("textarea").autocompleter("@", function () {
+      return _($("ul.users li").map(function (i, e) {
+        return $(e).text();
+      })).reject(function (nick) {
+        return nick === Talker.client.nick;
+      });
+    });
+  }
+
+  $(bindAutocompleter);
+
+
+}());
