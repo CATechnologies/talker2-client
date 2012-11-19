7103e8c186e500c0330934d25c72fc6b94d70aa2
Pablo <pablo@teambox.com>
2012-11-19 23:09:53 +0100

    Revert

--- js/client.js
+++ js/client.js
@@ -2,7 +2,7 @@ $(function () {
 
   var config = { nick: "user" + ~~(Math.random() * 1000)
                , channel: "#teambox"
-               , server: document.location.origin
+               , server: "talker-bridge.jit.su:80"
                };
 
   // get channel from url params
